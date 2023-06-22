df7a63b3880ccdda96125df7bf4c26150e10f83a
wrld <eelwrld@gmail.com>
2023-06-22 23:50:07 +0500

    start react prj

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+
+function App() {
+    const data = [
+        {name: 'John', salary: '800', increase: false},
+        {name: 'George', salary: '1500', increase: true},
+        {name: 'Endy', salary: '844550', increase: true},
+    ]
+
+    return (
+        <div className="app"> 
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>
+    );
+}
+
+export default App;
