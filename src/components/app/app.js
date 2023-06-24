fcdafaa91d2dcbfd3d81f23104263b2bde29238b
wrld <eelwrld@gmail.com>
2023-06-24 15:23:04 +0500

    work with states

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,12 +6,55 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
+// import {Component} from 'react';
+// class WhoAmI extends  Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             years: 27, 
+//             position: ''
+//         }
+//     }
+
+//     nextYear = () => {
+//         this.setState(state => ({
+//             years: state.years + 1
+//         }))
+//     }
+
+//     commitInputChanges = (e, color) => {
+//         console.log(color);
+//         this.setState({
+//             position: e.target.value
+//         })
+//     }
+// 
+//     render() {
+//         const {name, surname, link} = this.props;
+//         const {position, years} = this.state;
+//         return (
+//             <div>
+//                 <button onClick={this.nextYear}>+++</button>
+//                 <h1>My name is {name}, surname - {surname},
+//                     age - {years}, 
+//                     position - {position}
+//                 </h1>
+//                 <a href={link}>My profile</a>
+//                 <form>
+//                     <span>Введите должность</span>
+//                     <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}/>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
 
 function App() {
     const data = [
-        {name: 'John', salary: '800', increase: false},
-        {name: 'George', salary: '1500', increase: true},
-        {name: 'Endy', salary: '844550', increase: true},
+        {name: 'John', salary: '800', increase: false, id: 1},
+        {name: 'George', salary: '1500', increase: true, id: 2},
+        {name: 'Endy', salary: '844550', increase: true, id: 3},
     ]
 
     return (
@@ -25,6 +68,8 @@ function App() {
 
             <EmployeesList data={data}/>
             <EmployeesAddForm/>
+
+            {/* <WhoAmI name='dss' surname ='miller' link='dsds'/> */}
         </div>
     );
 }
